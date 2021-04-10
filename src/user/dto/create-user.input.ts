c77879a55a9a795d0b88fa98da8083af19e0200b
Denis Popazov <dpopazov@s-pro.io>
2021-04-11 00:29:34 +0300

    added validation

--- src/user/dto/create-user.input.ts
+++ src/user/dto/create-user.input.ts
@@ -1,11 +1,14 @@
 import { InputType, Field } from '@nestjs/graphql';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 @InputType()
 export class CreateUserInput {
 
     @Field()
+    @IsEmail()
     email: string;
 
     @Field()
+    @IsNotEmpty()
     password: string;
 }
