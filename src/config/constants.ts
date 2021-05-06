783b06ab8dca5567c3585810f20a3fbdf877c309
Denis Popazov <dpopazov@s-pro.io>
2021-05-06 23:41:19 +0300

    changed accessToken`s expiration

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,7 +3,7 @@ export const SALT_ROUNDS = 10;
 export const jwtConstants = {
   accessToken: {
     secret: 'accessSecretKey',
-    expiresIn: '30s',
+    expiresIn: '24h',
   },
   refreshToken: {
     secret: 'refreshSecretKey',
