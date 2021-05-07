6853c70998a36e3b9b0858533a27e50392b36184
Denis Popazov <dpopazov@s-pro.io>
2021-05-07 11:20:14 +0300

    added validation for todo

--- src/todo/dto/create-todo.input.ts
+++ src/todo/dto/create-todo.input.ts
@@ -1,10 +1,13 @@
 import { InputType, Field } from '@nestjs/graphql';
+import { IsNotEmpty } from 'class-validator';
 
 @InputType()
 export class CreateTodoInput {
   @Field()
+  @IsNotEmpty()
   title: string;
 
   @Field()
+  @IsNotEmpty()
   description: string;
 }
